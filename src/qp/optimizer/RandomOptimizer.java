dd2cc7af43e1321cfbf1a0f24d5389d03af3045a
jeffypie369 <jffersonzzz@hotmail.com>
2019-03-30 17:35:49 +0800

    update conflicts

--- src/qp/optimizer/RandomOptimizer.java
+++ src/qp/optimizer/RandomOptimizer.java
@@ -435,6 +435,8 @@ public class RandomOptimizer{
 	    return findNodeAt(((Select)node).getBase(),joinNum);
 	}else if(node.getOpType()==OpType.PROJECT){
 	    return findNodeAt(((Project)node).getBase(),joinNum);
+	}else if(node.getOpType()==OpType.DISTINCT){
+		return findNodeAt(((Distinct)node).getBase(),joinNum);
 	}else{
 	    return null;
 	}
@@ -462,6 +464,11 @@ public class RandomOptimizer{
 	    modifySchema(base);
 	    Vector attrlist = ((Project)node).getProjAttr();
 	    node.setSchema(base.getSchema().subSchema(attrlist));
+	}else if(node.getOpType()==OpType.DISTINCT){
+		Operator base = ((Distinct)node).getBase();
+		modifySchema(base);
+		Vector attrlist = ((Distinct)node).getProjAttr();
+		node.setSchema(base.getSchema().subSchema(attrlist));
 	}
     }
 
@@ -521,6 +528,10 @@ public class RandomOptimizer{
 	    Operator base = makeExecPlan(((Project)node).getBase());
 	    ((Project)node).setBase(base);
 	    return node;
+	}else if(node.getOpType() == OpType.DISTINCT){
+		Operator base = makeExecPlan(((Distinct)node).getBase());
+		((Distinct)node).setBase(base);
+		return node;
 	}else{
 	    return node;
 	}
