1caac7ac8fdd717db7eaabfd2e3477c547569f50
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2019-03-30 17:51:55 +0800

    Revert "First pull"
    This reverts commit a6d6fa45898fa50022eecceefeee78a1e7cfdd9d.

--- Component/build.bat
+++
@@ -1,7 +0,0 @@
-javac  -d %COMPONENT%\classes %COMPONENT%\src\qp\utils\*.java
-javac  -d %COMPONENT%\classes %COMPONENT%\src\qp\parser\*.java
-javac -d %COMPONENT%\classes %COMPONENT%\src\qp\operators\*.java
-javac -d %COMPONENT%\classes %COMPONENT%\src\qp\optimizer\*.java
-javac -d %COMPONENT%\classes %COMPONENT%\testcases\*.java
-javac -d %COMPONENT%\classes %COMPONENT%\src\QueryMain.java
-pause
--- Component/out/production/Component/abc.bat
+++
@@ -1,7 +0,0 @@
-java RandomDB CUSTOMER 100
-
-java ConvertTxtToTbl CUSTOMER
-
-java QueryMain query2 query2.out
-
-pause
