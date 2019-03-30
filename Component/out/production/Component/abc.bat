a6d6fa45898fa50022eecceefeee78a1e7cfdd9d
Jeevz10 <jeevanneralakeresomashekhar@gmail.com>
2019-03-30 17:43:44 +0800

    First pull

---
+++ Component/out/production/Component/abc.bat
@@ -0,0 +1,7 @@
+java RandomDB CUSTOMER 100
+
+java ConvertTxtToTbl CUSTOMER
+
+java QueryMain query2 query2.out
+
+pause
---
+++ Component/testcases/abc.bat
@@ -0,0 +1,10 @@
+java RandomDB CUSTOMER 100
+
+java ConvertTxtToTbl CUSTOMER
+java RandomDB CART 100
+
+java ConvertTxtToTbl CART
+
+java QueryMain query3 query3.out
+
+pause
